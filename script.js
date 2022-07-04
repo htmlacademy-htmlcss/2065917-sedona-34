50dcda45874bdd35bae6d106d9208d88a741283f
constoner <constantin.kalinin@gmail.com>
2022-07-05 00:40:30 +0300

    улучшил логику открытия/закрытия модалки

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let hotelSearhButton = document.querySelector(".hotel-search-button")
-let modalWindow = document.querySelector(".modal-container")
-let modalCloseButton = document.querySelector(".modal-close-button")
+const hotelSearhButton = document.querySelector(".hotel-search-button");
+const modalContainer = document.querySelector(".modal-container");
+const modalCloseButton = document.querySelector(".modal-close-button");
 
-hotelSearhButton.onclick = function() {
-  modalWindow.classList.add("modal-opened")
-};
+// переключатель состояния модального окна
+function modalToggle() {
+  if (modalContainer.classList.contains("modal-opened")) {
+    modalContainer.classList.remove("modal-opened");
+  } else {
+    modalContainer.classList.add("modal-opened");
+  }
+}
 
-modalCloseButton.onclick = function() {
-  modalWindow.classList.remove("modal-opened")
-};
+// открытие/закрытие модального окна по кнопкам
+hotelSearhButton.addEventListener("click", modalToggle);
+modalCloseButton.addEventListener("click", modalToggle);
+
+// закрытие окна по клику вне его с контролем нажатия/отпускания мыши
+var modalStateCheck = {};
+
+document.addEventListener("mousedown", function (e) {
+  modalStateCheck = e.target;
+});
+
+document.addEventListener("mouseup", function (e) {
+  if (modalStateCheck == e.target && modalStateCheck == modalContainer) {
+    modalToggle();
+  }
+  modalStateCheck = {};
+})
